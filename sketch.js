77afa228b2a5e2c0b2f3954b0b866466337d97a9
abha-coding <75477048+abha-coding@users.noreply.github.com>
2021-11-09 18:41:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background("black"); 
+  translate(200,200);
+  rotate(-90);
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr%12,0,12,0,360);
+
+  push();
+  rotate(scAngle);
+  stroke("red");
+  strokeWeight(10);
+  line(0,0,100,0)
+  pop();
+
+  push();
+  rotate(mnAngle);
+  stroke("yellow");
+  strokeWeight(10);
+  line(0,0,75,0)
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke("blue");
+  strokeWeight(10);
+  line(0,0,50,0)
+  pop();
+
+  stroke("red");
+  strokeWeight(10);
+  noFill();
+  arc(0,0,300,300,0,scAngle);
+
+  stroke("yellow");
+  arc(0,0,280,280,0,mnAngle);
+
+  stroke("blue");
+  arc(0,0,260,260,0,hrAngle);
+
+  drawSprites();
+}
